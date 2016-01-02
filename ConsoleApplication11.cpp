aa0ff8f30b75caf41763e69a72e235d3e61feb47
Metacious <gamer.distinction@gmail.com>
2016-01-02 00:33:56 -0300

    While practice
    Eternal loop

---
+++ ConsoleApplication11.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication11.cpp : main project file.
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	while (int i = 10)
+	{
+		cout << "Information repeats 10 times!" << endl;
+	}
+    return 0;
+}
---
+++ ConsoleApplication12.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication12.cpp : main project file.
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 9; i++)
+	{
+		cout << "More text, I'm hungry!!!!" << endl;
+	}
+	return 0;
+}
